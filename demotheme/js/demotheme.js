9de840e74cdb325d7964ad17773f62f71d2cdf70
Vanessa DAVID <vanessa.david@alten.com>
2019-11-26 09:24:15 +0100

    Add theme for correcting exercises

---
+++ demotheme/js/demotheme.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * DemoTheme behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.demotheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
