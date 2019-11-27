7e510eae4c1642c3a873b194d5e138c0a2bcb88b
Vanessa DAVID <vanessa.david@alten.com>
2019-11-27 12:30:22 +0100

    Add css and js script

--- demotheme/js/demotheme.js
+++ demotheme/js/demotheme.js
@@ -4,17 +4,13 @@
  */
 
 (function ($, Drupal) {
-
   'use strict';
-
   /**
    * Behavior description.
    */
   Drupal.behaviors.demotheme = {
     attach: function (context, settings) {
-
       console.log('It works!');
-
     }
   };
 
